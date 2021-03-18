8d242170fb396d5e60f19c03206db39dec6dcf0c
NaveenKumar <rnaveenk666@gmail.com>
2021-03-18 20:47:46 +0530

    delete customers on customer list re arranging the drawer page

--- lib/pages/customer page/card.dart
+++ lib/pages/customer page/card.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-import 'customerhome.dart';
 import 'customerlist.dart';
 
 class CardWidget extends StatefulWidget {
@@ -12,34 +11,81 @@ class CardWidget extends StatefulWidget {
 
 class _CardWidgetState extends State<CardWidget> {
   @override
+  void initState() {
+    super.initState();
+    Customer();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: ListView.builder(
           itemCount: Customer.getCustomers().length,
           itemBuilder: (context, index) {
             return Container(
-              child: ListTile(
-                  leading: CircleAvatar(radius: 20),
-                  title: Text(
-                    Customer.getCustomers().elementAt(index).name,
-                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-                  ),
-                  subtitle: Text(
-                    Customer.customers[index].phoneNumber,
-                    style: TextStyle(fontSize: 20),
-                  ),
-                  onTap: () {},
-                  trailing: IconButton(
-                    icon: Icon(Icons.delete),
-                    onPressed: () {
-                      Navigator.push(
-                          context,
-                          new MaterialPageRoute(
-                              builder: (context) => new CustomerHomeWidget()));
-                    },
-                  )),
-            );
+                child: ListTile(
+              leading: CircleAvatar(radius: 20),
+              title: Text(
+                Customer.getCustomers().elementAt(index).name,
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+              ),
+              subtitle: Text(
+                Customer.customers[index].phoneNumber,
+                style: TextStyle(fontSize: 20),
+              ),
+              onTap: () {},
+              trailing: IconButton(
+                  icon: Icon(Icons.delete),
+                  onPressed: () {
+                    showAlertDialog(context);
+                    /*setState(() {
+                      Customer.customers.removeAt(index);
+                    });*/
+                  }),
+            ));
           }),
     );
   }
+}
+
+showAlertDialog(BuildContext context) {
+  // set up the buttons
+  /*Widget yesButton = FlatButton(
+    child: Text("Yes"),
+    onPressed: () {
+      
+      
+    },
+  );
+  Widget noButton = FlatButton(
+    child: Text("No"),
+    onPressed: () {},
+  );*/
+
+  // set up the AlertDialog
+  AlertDialog alert = AlertDialog(
+    title: Text("AlertDialog"),
+    content: Text(
+        "Would you like to delete the customer details from the CUSTOMER PAGE"),
+    actions: [
+      FlatButton(
+        child: Text("Yes"),
+        onPressed: () {
+          // Customer.customers.removeAt(context);
+        },
+      ),
+      FlatButton(
+        child: Text("No"),
+        onPressed: () {},
+      )
+    ],
+  );
+
+  // show the dialog
+  showDialog(
+    context: context,
+    builder: (BuildContext context) {
+      return alert;
+    },
+  );
 }
---
+++ lib/pages/homepage/homepage.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:garments/pages/my_divider.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key key}) : super(key: key);
+
+  Widget build(BuildContext context) {
+    return Scaffold(
+      drawer: MyDivider(),
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text(
+          'Balaji Garments',
+          style: TextStyle(color: Colors.white),
+        ),
+      ),
+
+      /* Scaffold(
+        endDrawerEnableOpenDragGesture: true,
+        endDrawer: MyDivider(),
+      ),*/
+    );
+  }
+}
