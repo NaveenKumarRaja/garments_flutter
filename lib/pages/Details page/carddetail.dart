dc5a4de0bade8f5c95b97939edf468d968abafc4
NaveenKumar <rnaveenk666@gmail.com>
2021-04-07 19:31:19 +0530

    Detail page and editing page

---
+++ lib/pages/Details page/carddetail.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CardDetail extends StatefulWidget {
+  CardDetail({Key key}) : super(key: key);
+
+  @override
+  _CardDetailState createState() => _CardDetailState();
+}
+
+class _CardDetailState extends State<CardDetail> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
