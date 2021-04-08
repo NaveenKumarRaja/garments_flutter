6f00f1ee66401f4805537f44b40a1565a33f1683
NaveenKumar <rnaveenk666@gmail.com>
2021-04-08 12:45:06 +0530

    customer details in details page

--- lib/pages/Details page/carddetail.dart
+++ lib/pages/Details page/carddetail.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:garments/save%20and%20get/saved_details.dart';
 
 class CardDetail extends StatefulWidget {
   CardDetail({Key key}) : super(key: key);
@@ -10,12 +11,117 @@ class CardDetail extends StatefulWidget {
 class _CardDetailState extends State<CardDetail> {
   @override
   Widget build(BuildContext context) {
+    final CustomersForm customer = ModalRoute.of(context).settings.arguments;
+
     return Container(
       padding: EdgeInsets.fromLTRB(10, 10, 10, 10),
-      height: 300,
+      height: 220,
       width: double.maxFinite,
       child: Card(
         elevation: 5,
+        child: Padding(
+          padding: EdgeInsets.all(8),
+          child: Stack(
+            children: <Widget>[
+              Padding(
+                padding: EdgeInsets.only(left: 10, top: 5),
+                child: Column(
+                  children: <Widget>[
+                    Row(
+                      children: <Widget>[
+                        Icon(
+                          Icons.phone,
+                          size: 18,
+                        ),
+                        SizedBox(
+                          width: 10,
+                        ),
+                        Text(
+                          'Phone Number 1:' + '    ',
+                          style: TextStyle(fontSize: 15),
+                        ),
+                        Text(
+                          customer.phoneNumber,
+                          style: TextStyle(
+                              fontSize: 20, fontWeight: FontWeight.bold),
+                        )
+                      ],
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    Row(
+                      children: <Widget>[
+                        Icon(
+                          Icons.phone,
+                          size: 18,
+                        ),
+                        SizedBox(
+                          width: 10,
+                        ),
+                        Text(
+                          'Phone Number 2:' + '    ',
+                          style: TextStyle(fontSize: 15),
+                        ),
+                        Text(
+                          customer.phoneNo,
+                          style: TextStyle(
+                              fontSize: 20, fontWeight: FontWeight.bold),
+                        )
+                      ],
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    Row(
+                      children: <Widget>[
+                        Icon(
+                          Icons.location_on,
+                          size: 18,
+                        ),
+                        SizedBox(
+                          width: 10,
+                        ),
+                        Text(
+                          'Address                :' + '    ',
+                          style: TextStyle(fontSize: 15),
+                        ),
+                        Text(
+                          customer.address,
+                          style: TextStyle(
+                              fontSize: 20, fontWeight: FontWeight.bold),
+                        )
+                      ],
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    Row(
+                      children: <Widget>[
+                        Icon(
+                          Icons.location_city,
+                          size: 18,
+                        ),
+                        SizedBox(
+                          width: 10,
+                        ),
+                        Text(
+                          'City                        :' + '    ',
+                          style: TextStyle(fontSize: 15),
+                        ),
+                        Text(
+                          customer.city,
+                          style: TextStyle(
+                              fontSize: 20, fontWeight: FontWeight.bold),
+                        )
+                      ],
+                    )
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
