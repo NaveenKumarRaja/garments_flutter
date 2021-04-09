6112a4a9f8433ce4bdb340a7dbd3be737be5c0c4
NaveenKumar <rnaveenk666@gmail.com>
2021-04-09 19:30:16 +0530

    retrieve the values

--- lib/pages/Details page/editdetail.dart
+++ lib/pages/Details page/editdetail.dart
@@ -1,5 +1,9 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:garments/pages/Details%20page/detailspage.dart';
+import 'package:garments/pages/customer%20page/customerlist.dart';
+import 'package:garments/save%20and%20get/controller.dart';
 import 'package:garments/save%20and%20get/saved_details.dart';
 
 class EditDetails extends StatefulWidget {
@@ -10,19 +14,64 @@ class EditDetails extends StatefulWidget {
 }
 
 class _EditDetailsState extends State<EditDetails> {
+  final _form = GlobalKey<FormState>();
+  final _scafold = GlobalKey<ScaffoldState>();
+
   List<CustomersForm> customer = List<CustomersForm>();
+
   TextEditingController sNo = TextEditingController();
-  TextEditingController name = TextEditingController();
-  TextEditingController phoneNumber = TextEditingController();
-  TextEditingController phoneNo = TextEditingController();
-  TextEditingController address = TextEditingController();
-  TextEditingController city = TextEditingController();
+  TextEditingController name;
+  TextEditingController phoneNumber;
+  TextEditingController phoneNo;
+  TextEditingController address;
+  TextEditingController city;
+
+  get response => null;
+  @override
+  void initState() {
+    super.initState();
+    name = new TextEditingController();
+    phoneNumber = new TextEditingController();
+    phoneNo = new TextEditingController();
+    address = new TextEditingController();
+    city = new TextEditingController();
+  }
+
+  void _submitForm() {
+    if (_form.currentState.validate()) {
+      CustomersForm customersForm = CustomersForm(
+          name.text, phoneNumber.text, phoneNo.text, address.text, city.text);
+      FormController formController = FormController((String response) {
+        print(response);
+        if (response == null) {
+          _showSnackBar("CustomersForm Submitted");
+        } else {
+          _showSnackBar("Error Occured");
+        }
+      });
+      _showSnackBar("Submiiting Customer");
+      formController.submitForm(customersForm);
+    }
+    setState(() {
+      String jsonsDataString = response.toString();
+      customer = jsonDecode(jsonsDataString);
+      print(customer.toString());
+    });
+  }
+
+  _showSnackBar(String message) {
+    final snackBar = SnackBar(
+      content: Text(message),
+    );
+    _scafold.currentState.showSnackBar(snackBar);
+  }
 
   @override
   Widget build(BuildContext context) {
     final CustomersForm customer = ModalRoute.of(context).settings.arguments;
 
     return Scaffold(
+      key: _scafold,
       appBar: AppBar(
         //title: Text(customer.name),
         leading: IconButton(
@@ -45,12 +94,15 @@ class _EditDetailsState extends State<EditDetails> {
             height: 50,
           ),
           TextFormField(
-            controller: name,
+            initialValue: customer.name,
             decoration: InputDecoration(
                 border: OutlineInputBorder(),
                 icon: Icon(Icons.person),
                 hintText: 'Enter your name',
                 labelText: 'Name'),
+            onChanged: (String value) {
+              name = value as TextEditingController;
+            },
             validator: (String value) {
               if (value.isEmpty) {
                 return 'Please enter name';
@@ -63,7 +115,7 @@ class _EditDetailsState extends State<EditDetails> {
             height: 30,
           ),
           TextFormField(
-            controller: phoneNumber,
+            initialValue: customer.phoneNumber,
             decoration: const InputDecoration(
                 border: OutlineInputBorder(),
                 icon: const Icon(Icons.phone),
@@ -72,6 +124,9 @@ class _EditDetailsState extends State<EditDetails> {
             maxLength: 10,
             inputFormatters: [],
             keyboardType: TextInputType.phone,
+            onChanged: (String value) {
+              phoneNumber = value as TextEditingController;
+            },
             validator: (String value) {
               String patttern = r'^(?:[6-9])?[0-9]{10,12}$';
               RegExp regExp = new RegExp(patttern);
@@ -89,7 +144,7 @@ class _EditDetailsState extends State<EditDetails> {
             height: 11,
           ),
           TextFormField(
-            controller: phoneNo,
+            initialValue: customer.phoneNo,
             decoration: const InputDecoration(
                 border: OutlineInputBorder(),
                 icon: const Icon(Icons.phone_in_talk),
@@ -98,6 +153,9 @@ class _EditDetailsState extends State<EditDetails> {
             maxLength: 10,
             inputFormatters: [],
             keyboardType: TextInputType.phone,
+            onChanged: (String value) {
+              phoneNo = value as TextEditingController;
+            },
             validator: (String value) {
               String patttern = r'^(?:[6-9])?[0-9]{10,12}$';
               RegExp regExp = new RegExp(patttern);
@@ -115,12 +173,15 @@ class _EditDetailsState extends State<EditDetails> {
             height: 12,
           ),
           TextFormField(
-            controller: address,
+            initialValue: customer.address,
             decoration: const InputDecoration(
                 border: OutlineInputBorder(gapPadding: 20),
                 icon: const Icon(Icons.location_on),
                 hintText: 'Enter Address',
                 labelText: 'Address'),
+            onChanged: (String value) {
+              address = value as TextEditingController;
+            },
             validator: (String value) {
               if (value.isEmpty) {
                 return 'Please enter address';
@@ -132,12 +193,15 @@ class _EditDetailsState extends State<EditDetails> {
             height: 35,
           ),
           TextFormField(
-            controller: city,
+            initialValue: customer.city,
             decoration: const InputDecoration(
                 border: OutlineInputBorder(gapPadding: 20),
                 icon: const Icon(Icons.location_city),
                 hintText: 'Enter City',
                 labelText: 'City'),
+            onChanged: (String value) {
+              city = value as TextEditingController;
+            },
             validator: (String value) {
               if (value.isEmpty) {
                 return 'Please enter city';
@@ -166,14 +230,17 @@ class _EditDetailsState extends State<EditDetails> {
                       ),
                       FlatButton(
                         onPressed: () {
-                          setState(() {
-                            Navigator.push(
-                                context,
-                                new MaterialPageRoute(
-                                  builder: (context) => DetailsPage(),
-                                  settings: RouteSettings(arguments: customer),
-                                ));
-                          });
+                          if (!_form.currentState.validate()) {
+                            return;
+                          }
+                          _form.currentState.save();
+                          _submitForm();
+                          Navigator.push(
+                              context,
+                              new MaterialPageRoute(
+                                builder: (context) => DetailsPage(),
+                                settings: RouteSettings(arguments: customer),
+                              ));
                         },
                         child: Text("Yes"),
                       ),
