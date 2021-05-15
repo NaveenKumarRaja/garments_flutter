ffdfe11a0f3aeeddf128204e71e05eab6e3e5350
NaveenKumar <rnaveenk666@gmail.com>
2021-05-15 15:44:23 +0530

    tabBar

--- lib/pages/detail/Tabbar.dart
+++ lib/pages/detail/Tabbar.dart
@@ -1,73 +1,33 @@
 import 'package:flutter/material.dart';
 
-class Tabbar extends StatefulWidget {
-  Tabbar({Key key}) : super(key: key);
+class TabbarWidget extends StatefulWidget {
+  TabbarWidget({Key key}) : super(key: key);
 
   @override
-  _TabbarState createState() => _TabbarState();
+  _TabbarWidgetState createState() => _TabbarWidgetState();
 }
 
-class _TabbarState extends State<Tabbar>
-    with SingleTickerProviderStateMixin, RestorationMixin {
+class _TabbarWidgetState extends State<TabbarWidget>
+    with TickerProviderStateMixin {
   TabController _tabController;
-  final RestorableInt tabIndex = RestorableInt(0);
-
-  @override
-  String get restorationId => 'tab_scrollable_demo';
-
-  @override
-  void restoreState(RestorationBucket oldBucket, bool initialRestore) {
-    registerForRestoration(tabIndex, 'tab_index');
-    _tabController.index = tabIndex.value;
-  }
 
   @override
   void initState() {
-    _tabController = TabController(
-      initialIndex: 0,
-      length: 12,
-      vsync: this,
-    );
-    _tabController.addListener(() {
-      setState(() {
-        tabIndex.value = _tabController.index;
-      });
-    });
     super.initState();
-  }
-
-  @override
-  void dispose() {
-    _tabController.dispose();
-    tabIndex.dispose();
-    super.dispose();
+    _tabController = TabController(length: 2, vsync: this);
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('TabBar Widget'),
-        bottom: TabBar(
-          controller: _tabController,
-          tabs: const <Widget>[
-            Tab(
-              icon: Icon(Icons.cloud_outlined),
-            ),
-            Tab(
-              icon: Icon(Icons.beach_access_sharp),
-            ),
-            Tab(
-              icon: Icon(Icons.brightness_5_sharp),
-            ),
-          ],
-        ),
-      ),
-      body: TabBarView(
+      body: TabBar(
         controller: _tabController,
-        children: [
-          Center(
-            child: Text("e"),
+        tabs: <Widget>[
+          Tab(
+            text: "Items Sale",
+          ),
+          Tab(
+            text: "Bank Account",
           ),
         ],
       ),
